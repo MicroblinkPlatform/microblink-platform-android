6797147015cd61aa44d835751a4606bc4a236fe1
Matija Krizanec <matija.krizanec@microblink.com>
2025-07-31 14:37:34 +0200

    update dependencies

--- MicroblinkPlatformSample/app/build.gradle.kts
+++ MicroblinkPlatformSample/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.microblink.platform.sample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.microblink.platform.sample"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
